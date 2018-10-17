614d08bbf8ece369405fdb689a07a5f4f0aeff9e
dandelionroot <geteking@pusan.ac.kr>
2018-10-17 21:03:56 +0900

    Assignment 'rumor' is completed.

--- DataStructure/7-rumor/rumor.cpp
+++ DataStructure/7-rumor/rumor.cpp
@@ -1,78 +1,118 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define LINK_SIZE 50
 
 class Person {
-private:
+public:
 	int id;
-	int link[LINK_SIZE];
-	int linkNum;
+	vector< int > link;
 public:
 	Person(ifstream &inp);
-	int getId(void) const {
-		return this->id;
-	}
-	int* getLink(void) const {
-		return this->link;
-	}
-	int getLinkNum(void) const {
-		return this->linkNum;
-	}
 };
 
 Person::Person(ifstream &inp) {
-	int id, link, i;
-	memset(this->link, 0, sizeof(int)*LINK_SIZE);
-	inp >> id;
-	this->id = id;
-	for(i=0; inp>>link && !(link==0); ++i) {
-		this->link[i] = link;
+	inp >> this->id;
+	int link;
+	while(inp>>link && !(link==0)) {
+		this->link.push_back(link);
 	}
-	this->linkNum = --i;
+	sort(this->link.begin(), this->link.end());
 }
 
-auto getPerson(vector< Person > &people, int id) {
+class Listener {
+public:
+	int id;
+	int day;
+public:
+	Listener(int id, int day)
+		: id(id), day(day) {}
+};
+
+vector< Person >::iterator getPsIt(vector< Person > &people, int id) {
 	vector< Person >::iterator i;
 	for(i=people.begin(); i<people.end() &&
-		!(id==i->getId()); ++i)
+		!(i->id==id); ++i)
 		;
-	if(i<people.end())
-		return i;
-	else
-		cout << "error : index over" << endl;
-		return;
+	return i;
 }
 
-bool isRecord(vector< int > &record, int id)
+bool isRecord(vector< Listener > &record, int id) {
+	vector< Listener >::iterator i;
+	for(i=record.begin(); i<record.end() &&
+		!(id==i->id); ++i)
+		;
+	if(i<record.end())
+		return true;
+	else
+		return false;
+}
 
-void push(queue< int > &q, vector< int > &record, auto p) {
-	int i;
-	for(i=0; i<p->getLinkNum(); ++i) {
-		int id = p->getLink()[i];
-		if(!isRecord(record, id))
-			q.push(id);
+void moveQ(queue< int > &from, queue< int > &to) {
+	int i = from.front();
+	from.pop();
+	to.push(i);
+}
+
+void moveAndRecord(queue< int > &q, queue< int > &tq,
+	vector< Listener > &record, int day) {
+	while(q.size() != 0) {
+		int id = q.front();
+		moveQ(q, tq);
+		if(!isRecord(record, id)) {
+			Listener l(id, day);
+			record.push_back(l);
+		}
 	}
+}
+
+void push(queue< int > &q, queue< int > &tq,
+	vector< Person > &people, vector< Listener > &record) {
+	while(tq.size() != 0) {
+		int id = tq.front();
+		tq.pop(); 
+		auto it = getPsIt(people, id);
+		for(auto i=it->link.begin(); i<it->link.end(); ++i) {
+			if(!isRecord(record, *i))
+				q.push(*i);
+		}
+	}
+}
+
+void printListener(vector< Listener > &record,
+	ofstream &out) {
+	for(auto i=record.begin(); i<record.end(); ++i)
+		if(i == record.end()-1)
+			out << i->id;
+		else
+			out << i->id << endl;
 }
 
 int main()
 {
-	int N, x, d, i;
+	int N, x, d, i, day=1;
 	vector< Person > people;
-	queue< int > q;
-	vector< int > record;
+	queue< int > q, tq;
+	vector< Listener > record;
 	ifstream inp("rumor.inp");
 	ofstream out("rumor.out");
 	inp >> N >> x >> d;
-
 	for(i=0; i<N; ++i) {
 		Person p(inp);
 		people.push_back(p);
 	}
 
-	auto p = getPerson(people, id);
-	do {
-		q.push(q, record, p);
-	} while(q.size() == 0)
+	auto it = getPsIt(people, x);
+	q.push(it->id);
+	for(auto i=it->link.begin(); i<it->link.end(); ++i) {
+		q.push(*i);
+	}
+
+	while(day <= d) {
+		moveAndRecord(q, tq, record, day);
+		push(q, tq, people, record);
+		++day;
+	}
+
+	printListener(record, out);
 
 	inp.close();
 	out.close();
